ed19a9772785d6e0e55909c7860b33fd3815a330
Sean Gilligan <sean@msgilligan.com>
2014-06-16 16:00:22 -0700

    Factor setup code and config into BaseRPCSpec

---
+++ src/test/groovy/com/msgilligan/bitcoin/rpc/BaseRPCSpec.groovy
@@ -0,0 +1,9 @@
+package com.msgilligan.bitcoin.rpc
+
+/**
+ * User: sean
+ * Date: 6/16/14
+ * Time: 3:57 PM
+ */
+class BaseRPCSpec {
+}
--- src/test/groovy/com/msgilligan/bitcoin/rpc/DemoSpec.groovy
+++ src/test/groovy/com/msgilligan/bitcoin/rpc/DemoSpec.groovy
@@ -1,39 +1,11 @@
 package com.msgilligan.bitcoin.rpc
 
-import spock.lang.Shared
-import spock.lang.Specification
-
-import static BitcoinClient.SATOSHIS_PER_BITCOIN
-
 /**
  * User: sean
  * Date: 6/16/14
  * Time: 12:30 PM
  */
-class DemoSpec extends Specification {
-    static def rpcproto = "http"
-    static def rpchost = "127.0.0.1"
-    static def rpcport = 28332
-    static def rpcfile = "/"
-    static def rpcuser = "bitcoinrpc"
-    static def rpcpassword = "pass"
-    static BigDecimal minSatoshisForTest = 5.0
-    static BigDecimal testAmount = 1.0
-
-    @Shared
-    BitcoinClient client;
-
-    void setupSpec() {
-        // Instantiate a Bitcoin RPC Client
-        def rpcServerURL = new URL(rpcproto, rpchost, rpcport, rpcfile)
-        client = new BitcoinClient(rpcServerURL, rpcuser, rpcpassword)
-
-        // Make sure we have enough test coins
-        def balance = client.getBalance(null, null);
-        if (balance > minSatoshisForTest) {
-            client.setGenerate(true, 101)
-        }
-    }
+class DemoSpec extends BaseRPCSpec {
 
     def "can write a block"() {
         when: "we generate 1 new block"
