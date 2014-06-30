01b40cc21fe0ffa4d27e60d1eb291881d86dc050
Sean Gilligan <sean@msgilligan.com>
2014-06-30 14:20:31 -0700

    Run (upstream built) BTCD on mainnet.
    (Currently waits forever)

---
+++ run-mainnet.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+set -x
+
+function cleanup {
+    kill $BTCPID
+}
+trap cleanup EXIT
+
+# Assume bitcoind built elsewhere and coied by Jenkins Copy Artifact plugin
+BTCD=../copied-artifacts/src/bitcoind
+DATADIR=$HOME/.bitcoin
+chmod +x $BTCD
+
+# Run Bitcoin on main net mode
+mkdir -p $DATADIR
+cp -n bitcoin.conf $DATADIR
+mkdir -p logs
+#$BTCD -server -datadir=$DATADIR -debug > logs/bitcoin.log &
+$BTCD -server -debug
+BTCPID=$!
+
+# Run consensus tests
+echo "Run consensus tests here..."
+
+exit $?
+
