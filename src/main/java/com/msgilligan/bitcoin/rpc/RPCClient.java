8ce32ef8f433b2fd49b219ba64b655a807b4c59c
Sean Gilligan <sean@msgilligan.com>
2014-09-28 20:17:50 -0700

    Disable checks that prevent using self-signed SSL
    TODO: Should these be enabled by default for security?

--- src/main/java/com/msgilligan/bitcoin/rpc/RPCClient.java
+++ src/main/java/com/msgilligan/bitcoin/rpc/RPCClient.java
@@ -2,6 +2,12 @@ package com.msgilligan.bitcoin.rpc;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSession;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
@@ -9,6 +15,9 @@ import java.net.Authenticator;
 import java.net.HttpURLConnection;
 import java.net.PasswordAuthentication;
 import java.net.URL;
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.X509Certificate;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
@@ -25,6 +34,15 @@ public class RPCClient {
     private HttpURLConnection connection;
     private ObjectMapper mapper;
     private long requestId;
+    private static final boolean disableSslVerification = true;
+
+    static {
+        if (disableSslVerification) {
+            // Disable checks that prevent using a self-signed SSL certificate
+            // TODO: Should checks be enabled by default for security reasons?
+            disableSslVerification();
+        }
+    }
 
     public RPCClient(RPCConfig config) {
         this(config.getUrl(), config.getUsername(), config.getPassword());
@@ -142,5 +160,41 @@ public class RPCClient {
 
     private void closeConnection() {
         connection.disconnect();
+    }
+
+    private static void disableSslVerification() {
+        try
+        {
+            // Create a trust manager that does not validate certificate chains
+            TrustManager[] trustAllCerts = new TrustManager[] {new X509TrustManager() {
+                public java.security.cert.X509Certificate[] getAcceptedIssuers() {
+                    return null;
+                }
+                public void checkClientTrusted(X509Certificate[] certs, String authType) {
+                }
+                public void checkServerTrusted(X509Certificate[] certs, String authType) {
+                }
+            }
+            };
+
+            // Install the all-trusting trust manager
+            SSLContext sc = SSLContext.getInstance("SSL");
+            sc.init(null, trustAllCerts, new java.security.SecureRandom());
+            HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
+
+            // Create all-trusting host name verifier
+            HostnameVerifier allHostsValid = new HostnameVerifier() {
+                public boolean verify(String hostname, SSLSession session) {
+                    return true;
+                }
+            };
+
+            // Install the all-trusting host verifier
+            HttpsURLConnection.setDefaultHostnameVerifier(allHostsValid);
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        } catch (KeyManagementException e) {
+            e.printStackTrace();
+        }
     }
 }
