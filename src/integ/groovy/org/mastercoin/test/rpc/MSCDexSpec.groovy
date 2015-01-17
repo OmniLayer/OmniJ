901f44b313fde64aca545ae983809f78a375daf8
dexX7 <ugithub@bitwatch.co>
2015-01-17 22:24:44 +0100

    MSCDexSpec: more compact data providing

--- src/integ/groovy/org/mastercoin/test/rpc/MSCDexSpec.groovy
+++ src/integ/groovy/org/mastercoin/test/rpc/MSCDexSpec.groovy
@@ -15,10 +15,11 @@ class MSCDexSpec extends BaseRegTestSpec {
 
     final static BigDecimal stdCommitFee = 0.0001
     final static BigInteger stdBlockSpan = 10
+    final static Short actionNew = 1
 
-    def "The generated hex-encoded transaction matches a valid reference transaction" () {
+    def "The generated hex-encoded transaction matches a valid reference transaction"() {
         given:
-        def txHex = createDexSellOfferHex(MSC, 1.0, 0.2, stdBlockSpan, stdCommitFee, 1)
+        def txHex = createDexSellOfferHex(MSC, 1.0, 0.2, 10, 0.0001, 1)
         def expectedHex = "00010014000000010000000005f5e1000000000001312d000a000000000000271001"
 
         expect:
@@ -27,11 +28,6 @@ class MSCDexSpec extends BaseRegTestSpec {
 
     def "A new sell offer can be created with Action = 1 (New)"() {
         given:
-        def startBTC = 0.1
-        def startMSC = 2.5
-        def currencyOffered = MSC
-        def amountOffered = 1.0
-        def desiredBTC = 0.2
         def action = 1
         def fundedAddress = createFundedAddress(startBTC, startMSC)
         def activeOffersAtTheStart = getactivedexsells_MP()
@@ -49,6 +45,9 @@ class MSCDexSpec extends BaseRegTestSpec {
         and: "a new offer is created on the distributed exchange"
         def activeOffersNow = getactivedexsells_MP()
         activeOffersNow.size() == activeOffersAtTheStart.size() + 1
+
+        where:
+        [startBTC, startMSC, currencyOffered, amountOffered, desiredBTC] << [[0.1, 2.5, MSC, 1.0, 0.2]]
     }
 
     @Unroll
@@ -58,12 +57,11 @@ class MSCDexSpec extends BaseRegTestSpec {
         def startMSC = 2.5
         def amountOffered = 1.0
         def desiredBTC = 0.2
-        def action = 1
         def fundedAddress = createFundedAddress(startBTC, startMSC)
 
         when: "an offer of #currencyId is created"
         def offerTxid = createDexSellOffer(
-                fundedAddress, currencyOffered, amountOffered, desiredBTC, stdBlockSpan, stdCommitFee, action)
+                fundedAddress, currencyOffered, amountOffered, desiredBTC, stdBlockSpan, stdCommitFee, actionNew)
         generateBlock()
 
         then: "the transaction should be a valid offering of #currencyOffered"
@@ -77,73 +75,57 @@ class MSCDexSpec extends BaseRegTestSpec {
 
     def "Offering more tokens than available puts up an offer with the available amount"() {
         given:
-        def startBTC = 0.1
-        def startMSC = 2.5
-        def currencyOffered = MSC
-        def desiredBTC = 50.0
-        def action = 1
         def fundedAddress = createFundedAddress(startBTC, startMSC)
-
-        when: "the amount offered for sale exceeds the sending address's available balance"
         def amountAvailableAtStart = getbalance_MP(fundedAddress, currencyOffered).balance
         def amountOffered = amountAvailableAtStart + 100.0
 
+        when: "the amount offered for sale exceeds the sending address's available balance"
         def offerTxid = createDexSellOffer(
-                fundedAddress, currencyOffered, amountOffered, desiredBTC, stdBlockSpan, stdCommitFee, action)
+                fundedAddress, currencyOffered, amountOffered, desiredBTC, stdBlockSpan, stdCommitFee, actionNew)
         generateBlock()
 
         then: "this indicates to sell all tokens that are available"
         def offerTx = getTransactionMP(offerTxid)
         def offerAmount = new BigDecimal(offerTx.amount)
         def amountAvailableNow = getbalance_MP(fundedAddress, currencyOffered).balance
-
         offerTx.valid == true
         offerAmount == amountAvailableAtStart
         offerAmount == Math.min(amountAvailableAtStart, amountOffered)
         amountAvailableNow == 0.0
+
+        where:
+        [startBTC, startMSC, currencyOffered, desiredBTC] << [[0.1, 2.5, MSC, 50.0]]
     }
 
     def "The amount offered for sale is reserved from the available balance"() {
         given:
-        def startBTC = 0.1
-        def startMSC = 100.0
-        def currencyOffered = MSC
-        def amountOffered = 90.0
-        def desiredBTC = 45.0
-        def action = 1
         def fundedAddress = createFundedAddress(startBTC, startMSC)
         def balanceAtStart = getbalance_MP(fundedAddress, currencyOffered)
 
         when: "an amount is offered for sale"
         def offerTxid = createDexSellOffer(
-                fundedAddress, currencyOffered, amountOffered, desiredBTC, stdBlockSpan, stdCommitFee, action)
+                fundedAddress, currencyOffered, amountOffered, desiredBTC, stdBlockSpan, stdCommitFee, actionNew)
         generateBlock()
 
         then: "the offered amount is reserved and subtracted from the available balance"
         def offerTx = getTransactionMP(offerTxid)
         def offerAmount = new BigDecimal(offerTx.amount)
         def balanceNow = getbalance_MP(fundedAddress, currencyOffered)
-
         offerTx.valid == true
         balanceNow.balance == balanceAtStart.balance - offerAmount
         balanceNow.reserved == balanceAtStart.reserved + offerAmount
+
+        where:
+        [startBTC, startMSC, currencyOffered, amountOffered, desiredBTC] << [[0.1, 100.0, MSC, 90.0, 45.0]]
     }
 
     def "Receiving tokens doesn't increase the offered amount of a published offer"() {
         given:
-        def startBTC = 0.1
-        def startMSC  = 2.5
-        def currencyOffered = MSC
-        def offerMSC = 90.0
-        def desiredBTC = 45.0
-        def action = 1
-        def startOtherMSC = 10.0
-        def additionalMSC = 10.0
         def fundedAddress = createFundedAddress(startBTC, startMSC)
 
         when: "the sell offer is published"
         def offerTxid = createDexSellOffer(
-                fundedAddress, currencyOffered, offerMSC, desiredBTC, stdBlockSpan, stdCommitFee, action)
+                fundedAddress, currencyOffered, offerMSC, desiredBTC, stdBlockSpan, stdCommitFee, actionNew)
         generateBlock()
 
         and: "additional tokens are received"
@@ -165,32 +147,31 @@ class MSCDexSpec extends BaseRegTestSpec {
         offerNow.valid == true
         offerNow.amount == offerBeforeReceivingMore.amount
         balanceNow.reserved == balanceBeforeReceivingMore.reserved
+
+        where:
+        [startBTC, startMSC, currencyOffered, offerMSC, desiredBTC,
+         startOtherMSC, additionalMSC] << [[0.1, 2.5, MSC, 90.0, 45.0, 10.0, 10.0]]
     }
 
     def "There can be only one active offer that accepts BTC"() {
         given:
-        def startBTC = 0.1
-        def startMSC  = 2.5
-        def currencyOffered = MSC
-        def firstOfferMSC = 1.0
-        def firstOfferBTC = 0.2
-        def secondOfferMSC = 1.5
-        def secondOfferBTC = 0.3
-        def action = 1
         def fundedAddress = createFundedAddress(startBTC, startMSC)
 
         when: "there is already an active offer accepting BTC"
         def firstOfferTxid = createDexSellOffer(
-                fundedAddress, currencyOffered, firstOfferMSC, firstOfferBTC, stdBlockSpan, stdCommitFee, action)
+                fundedAddress, currencyOffered, firstOfferMSC, firstOfferBTC, stdBlockSpan, stdCommitFee, actionNew)
         generateBlock()
 
         then: "creating any other offer accepting BTC is rejected"
         def secondOfferTxid = createDexSellOffer(
-                fundedAddress, currencyOffered, secondOfferMSC, secondOfferBTC, stdBlockSpan, stdCommitFee, action)
+                fundedAddress, currencyOffered, secondOfferMSC, secondOfferBTC, stdBlockSpan, stdCommitFee, actionNew)
         generateBlock()
-
         getTransactionMP(firstOfferTxid).valid == true
         getTransactionMP(secondOfferTxid).valid == false
+
+        where:
+        [startBTC, startMSC, currencyOffered, firstOfferMSC, firstOfferBTC,
+         secondOfferMSC, secondOfferBTC] << [[0.1, 2.5, MSC, 1.0, 0.2, 1.5, 0.3]]
     }
 
 }
