aa6933004ba21e82ad75aa4d0f42144dee88e747
Sean Gilligan <sean@msgilligan.com>
2015-02-03 09:11:15 -0800

    Cleanup shell scripts in project root
    * Delete 2
    * Move 3 to src/scripts
    
    The 3 that remain in the root are the three that are currently used by
    Jenkins jobs:
    https://github.com/msgilligan/bitcoin-spock#running-the-tests-from-jenkins

--- server.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-set -x
-BTCD=bitcoind
-mkdir -p regtest-datadir
-cp bitcoin.conf regtest-datadir
-$BTCD -server -regtest -datadir=regtest-datadir -debug
-
--- setup-client.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-set -x
-DATADIR=bitcoin-datadir
-CLI="copied-artifacts/src/bitcoin-cli -regtest -datadir=$DATADIR"
-$CLI setgenerate true 101 
-$CLI getblockcount
