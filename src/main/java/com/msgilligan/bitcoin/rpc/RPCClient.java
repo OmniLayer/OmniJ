4a23df2735abc110958b14aafb2b0fb0746a350f
Sean Gilligan <sean@msgilligan.com>
2015-03-11 17:40:01 -0700

    Simplify and improve RPCClient.send()

--- src/main/java/com/msgilligan/bitcoin/rpc/RPCClient.java
+++ src/main/java/com/msgilligan/bitcoin/rpc/RPCClient.java
@@ -1,6 +1,8 @@
 package com.msgilligan.bitcoin.rpc;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.net.ssl.HostnameVerifier;
 import javax.net.ssl.HttpsURLConnection;
@@ -32,6 +34,7 @@ import java.util.Scanner;
  * JSON-RPC Client
  */
 public class RPCClient {
+    private static final Logger log = LoggerFactory.getLogger(BitcoinClient.class);
     private URI serverURI;
     private String username;
     private String password;
@@ -63,73 +66,74 @@ public class RPCClient {
         return serverURI;
     }
 
-    protected Map<String, Object> send(Map<String, Object> request) throws IOException, JsonRPCException {
+    /**
+     * Send a JSON-RPC request to the server and return a response.
+     *
+     * @param request JSON-RPC request in Map format
+     * @return
+     * @throws IOException when thrown by the underlying HttpURLConnection
+     * @throws JsonRPCStatusException when the HTTP response code is other than 200
+     */
+    protected Map<String, Object> send(Map<String, Object> request) throws IOException, JsonRPCStatusException {
         HttpURLConnection connection = openConnection();
 
+        // TODO: Make sure HTTP keep-alive will work
+        // See: http://docs.oracle.com/javase/7/docs/technotes/guides/net/http-keepalive.html
+        // http://developer.android.com/reference/java/net/HttpURLConnection.html
+        // http://android-developers.blogspot.com/2011/09/androids-http-clients.html
+
+        if (log.isInfoEnabled()) {
+            log.info("Req json: {}", mapper.writeValueAsString(request));
+        }
 
         OutputStream requestStream = connection.getOutputStream();
-        String reqString = mapper.writeValueAsString(request);
-//        System.out.println("Req json = " + reqString);
-         try {
-             mapper.writeValue(requestStream, request);
-             requestStream.close();
-         } catch (IOException e) {
-             e.printStackTrace();
-             throw new JsonRPCException("IOException writing request stream", e);
-         }
+        mapper.writeValue(requestStream, request);
+        requestStream.close();
 
-        InputStream responseStream = null;
-        int code = connection.getResponseCode();
-        String message = connection.getResponseMessage();
-//        System.out.println("Response code: " + code);
-        if (code == 200) {
-            try {
-                responseStream = connection.getInputStream();
-            } catch (IOException e) {
-                e.printStackTrace();
-                throw new JsonRPCException("IOException reading response stream", e);
-            }
+        int responseCode = connection.getResponseCode();
+        log.info("Response code: {}", responseCode);
+
+        Map<String, Object> responseJson = null;
+        if (responseCode == 200) {
+            // Read JSON and return responseJson
+            responseJson = mapper.readValue(connection.getInputStream(), Map.class);
         } else {
-            responseStream = connection.getErrorStream();
-        }
-        String contentType = connection.getContentType();
-
-        String responseString;
-        Map<String, Object> responseMap = null;
-        if (responseStream != null) {
-            responseString = new Scanner(responseStream,"UTF-8").useDelimiter("\\A").next();
-            if (contentType.equals("application/json")) {
-                responseMap = mapper.readValue(responseString, Map.class);
-            }
-        } else {
-            responseString = "";
-            responseMap = new HashMap<String, Object>();
-        }
-
-        if (code != 200) {
-            String exceptionMessage = message; // Default to HTTP result message
-
-            if (responseMap != null) {
-                Map <String, Object> error = (Map <String, Object>) responseMap.get("error");
+            // Prepare and throw JsonRPCStatusException with all relevant info
+            String responseMessage = connection.getResponseMessage();
+            String exceptionMessage = responseMessage;
+            Map<String, Object> bodyJson = null;    // Body as JSON if available
+            String bodyString = null;               // Body as String if not JSON
+            if (connection.getContentType().equals("application/json")) {
+                // We got a JSON error response, parse it
+                bodyJson = mapper.readValue(connection.getErrorStream(), Map.class);
+                Map <String, Object> error = (Map <String, Object>) bodyJson.get("error");
                 if (error != null) {
                     // If there's a more specific message in the JSON use it instead.
                     exceptionMessage = (String) error.get("message");
                 }
+            } else {
+                // No JSON, read response body as string
+                InputStream errorStream = connection.getErrorStream();
+                bodyString = new Scanner(errorStream,"UTF-8").useDelimiter("\\A").next();
+                errorStream.close();
             }
-            throw new JsonRPCStatusException(exceptionMessage, code, message, responseString, responseMap);
+            throw new JsonRPCStatusException(exceptionMessage, responseCode, responseMessage, bodyString, bodyJson);
         }
 
+        log.info("Resp json: {}", responseJson);
+
         connection.disconnect();
-        return responseMap;
+        return responseJson;
     }
 
-    protected Map<String, Object> send(String method, List<Object> params) throws IOException, JsonRPCException  {
+    protected Map<String, Object> send(String method, List<Object> params) throws IOException, JsonRPCStatusException {
         Map<String, Object> request = new HashMap<String, Object>();
         request.put("jsonrpc", "1.0");
         request.put("method", method);
         request.put("id", Long.toString(requestId));
 
         if (params != null) {
+            // TODO: Should only remove nulls from the end
             params.removeAll(Collections.singleton(null));  // Remove null entries (should only be at end)
         }
         request.put("params", params);
