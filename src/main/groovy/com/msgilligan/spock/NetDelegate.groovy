50788dc664360bf886aea90d75472eaa95208086
Sean Gilligan <sean@msgilligan.com>
2015-03-17 16:19:35 -0700

    Remove demo/test Groovy trait/delegate test impl

--- src/main/groovy/com/msgilligan/spock/NetDelegate.groovy
+++
@@ -1,8 +0,0 @@
-package com.msgilligan.spock
-
-/**
- * Demonstration delegating trait for testing Groovy traits and delegates with Spock
- */
-trait NetDelegate {
-    @Delegate NetClient client = new NetClient()
-}
--- src/main/groovy/com/msgilligan/spock/package-info.java
+++
@@ -1,4 +0,0 @@
-/**
- * Demonstration classes for testing Groovy traits adding delegates to Spock Specs
- */
-package com.msgilligan.spock;
