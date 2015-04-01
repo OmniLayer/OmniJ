cccef15e8b36a4a6e8e3d0e47501718f05991a69
Sean Gilligan <sean@msgilligan.com>
2015-04-01 01:08:05 -0700

    Oops. Add the feature test this Spec was created for.

--- omnij-rpc/src/test/groovy/foundation/omni/rpc/RawTxBuilderSpec.groovy
+++ omnij-rpc/src/test/groovy/foundation/omni/rpc/RawTxBuilderSpec.groovy
@@ -1,11 +1,27 @@
 package foundation.omni.rpc
 
+import spock.lang.Shared
 import spock.lang.Specification
+
+import static foundation.omni.CurrencyID.MSC
 
 
 /**
- *
+ * RawTxBuilder unit tests
  */
 class RawTxBuilderSpec extends Specification {
+    @Shared
+    RawTxBuilder builder
 
+    def setup() {
+        builder = new RawTxBuilder()
+    }
+
+    def "The generated hex-encoded transaction matches a valid reference transaction"() {
+        when:
+        def txHex = builder.createDexSellOfferHex(MSC, 1.0, 0.2, (Byte) 10, 0.0001, (Byte) 1)
+
+        then:
+        txHex == "00010014000000010000000005f5e1000000000001312d000a000000000000271001"
+    }
 }
