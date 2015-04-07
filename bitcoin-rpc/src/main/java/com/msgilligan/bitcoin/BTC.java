641f2ef7469941731330244bd502474a97200c0d
Sean Gilligan <sean@msgilligan.com>
2015-04-06 22:10:54 -0700

    Add conversions between BigDecimal & bitcoinj Coin

--- bitcoin-rpc/src/main/java/com/msgilligan/bitcoin/BTC.java
+++ bitcoin-rpc/src/main/java/com/msgilligan/bitcoin/BTC.java
@@ -1,5 +1,7 @@
 package com.msgilligan.bitcoin;
 
+import org.bitcoinj.core.Coin;
+
 import java.math.BigDecimal;
 import java.math.BigInteger;
 import java.math.MathContext;
@@ -19,5 +21,12 @@ public class BTC  {
     public static BigInteger btcToSatoshis(BigDecimal btc) {
         BigDecimal satoshisDecimal = btc.multiply(BTC.satoshisPerBTCDecimal);
         return satoshisDecimal.toBigInteger();
+    }
+    public static Coin btcToCoin(BigDecimal btc) {
+        return Coin.valueOf(BTC.btcToSatoshis(btc).longValue());
+    }
+    public static BigDecimal coinToBTC(Coin coin) {
+        BigInteger satoshis = BigInteger.valueOf(coin.longValue());
+        return BTC.satoshisToBTC(satoshis);
     }
 }
