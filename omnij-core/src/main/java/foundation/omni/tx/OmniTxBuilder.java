1615c1a2657a6c8b94ecc2eebd514b143f538d89
Sean Gilligan <sean@msgilligan.com>
2015-04-07 16:41:21 -0700

    OmniTxBuilder WIP with lame-O test spec

---
+++ omnij-core/src/main/java/foundation/omni/tx/OmniTxBuilder.java
@@ -0,0 +1,89 @@
+package foundation.omni.tx;
+
+import foundation.omni.CurrencyID;
+import foundation.omni.net.OmniNetworkParameters;
+import foundation.omni.net.OmniRegTestParams;
+import org.bitcoinj.core.Address;
+import org.bitcoinj.core.Coin;
+import org.bitcoinj.core.ECKey;
+import org.bitcoinj.core.NetworkParameters;
+import org.bitcoinj.core.Transaction;
+import org.bitcoinj.core.TransactionOutput;
+import org.bitcoinj.params.RegTestParams;
+
+import java.util.List;
+
+/**
+ * Builds signed Omni transactions in bitcoinj Transaction objects
+ *
+ * TODO: ability to create unsigned transactions
+ *
+ */
+public class OmniTxBuilder {
+    private final long stdTxFee = 10_000;
+    private final NetworkParameters netParams = RegTestParams.get();
+    private final OmniNetworkParameters omniParams = OmniRegTestParams.get();
+    private final RawTxBuilder builder = new RawTxBuilder();
+
+    public Transaction createSignedSimpleSend(ECKey fromKey, List<TransactionOutput> unspentOutputs, Address toAddress, CurrencyID currencyID, long amount) {
+        String txHex = builder.createSimpleSendHex(currencyID, amount);
+        byte[] payload = RawTxBuilder.hexToBinary(txHex);
+        return createSignedOmniTransaction(fromKey, unspentOutputs, toAddress, payload);
+    }
+
+    /**
+     * Create a signed Omni transaction in a bitcoinj Transaction object
+     *
+     * @param fromKey Private key/address to send from
+     * @param unspentOutputs A list of unspent outputs for funding the transaction
+     * @param refAddress The Omni reference address (for the reference output)
+     * @param payload Omni transaction payload as a raw byte array
+     * @return Signed and ready-to-send Transaction
+     */
+    public Transaction createSignedOmniTransaction(ECKey fromKey, List<TransactionOutput> unspentOutputs, Address refAddress, byte[] payload) {
+        Address fromAddress = fromKey.toAddress(netParams);
+
+        // Encode the Omni Protocol Payload as a Class B transaction
+        Transaction tx = EncodeMultisig.encodeObfuscated(fromKey, payload, fromAddress.toString());
+
+        // Add outputs to the transaction
+        tx.addOutput(Coin.MILLICOIN, omniParams.getExodusAddress());    // Add correct Exodus Output
+        tx.addOutput(Coin.CENT, refAddress);                            // Reference (destination) address output
+
+        // Calculate change
+        long amountIn     = sum(unspentOutputs);    // Sum of available UTXOs
+        long amountOut    = sum(tx.getOutputs());   // Sum of outputs, this transaction
+        long amountChange = amountIn - amountOut - stdTxFee;
+        // If change is negative, transaction is invalid
+        if (amountChange < 0) {
+            // TODO: Throw Exception
+            System.out.println("Insufficient funds");
+        }
+        // If change is positive, return it all the the sending address
+        if (amountChange > 0) {
+            // Add a change output
+            tx.addOutput(Coin.valueOf(amountChange), fromAddress);
+        }
+
+        // Add all UTXOs for fromAddress as inputs
+        for (TransactionOutput output : unspentOutputs) {
+            tx.addSignedInput(output, fromKey);
+        }
+
+        return tx;
+    }
+
+    /**
+     * Calculate the total value of a list of transaction outputs.
+     *
+     * @param outputs list of transaction outputs to total
+     * @return total value in satoshis
+     */
+    long sum(List <TransactionOutput> outputs) {
+        long sum = 0;
+        for (TransactionOutput output : outputs) {
+            sum += output.getValue().longValue();
+        }
+        return sum;
+    }
+}
---
+++ omnij-core/src/test/groovy/foundation/omni/tx/BitcoinJTransactionBuilderSpec.groovy
@@ -0,0 +1,82 @@
+package foundation.omni.tx
+
+import foundation.omni.net.OmniMainNetParams
+import foundation.omni.net.OmniRegTestParams
+import org.bitcoinj.core.Coin
+import org.bitcoinj.core.ECKey
+import org.bitcoinj.core.Sha256Hash
+import org.bitcoinj.core.Transaction
+import org.bitcoinj.core.TransactionInput
+import org.bitcoinj.core.TransactionOutPoint
+import org.bitcoinj.core.TransactionOutput
+import org.bitcoinj.params.MainNetParams
+import org.bitcoinj.params.RegTestParams
+import org.bitcoinj.script.Script
+import org.bitcoinj.script.ScriptBuilder
+import org.spongycastle.util.encoders.Hex
+import spock.lang.Specification
+
+import static foundation.omni.CurrencyID.MSC
+
+
+/**
+ * WIP: Unit tests of building transactions
+ * This code is currently really poor - not to be used as an example
+ *
+ * TODO: Make this test into a real test
+ *
+ */
+class BitcoinJTransactionBuilderSpec extends Specification {
+    static final RawTxBuilder builder = new RawTxBuilder()
+    static final OmniTxBuilder omniTxBuilder = new OmniTxBuilder()
+
+    static final netParams = MainNetParams.get()
+    static final mpParams = OmniMainNetParams.get()
+    final static BigInteger NotSoPrivatePrivateKey = new BigInteger(1, Hex.decode("180cb41c7c600be951b5d3d0a7334acc7506173875834f7a6c4c786a28fcbb19"));
+    static final senderKey = ECKey.fromPrivate(NotSoPrivatePrivateKey, false)
+    static final senderAddr = senderKey.toAddress(netParams)
+
+    def "Build a transaction using EncodeMultisig.encodeObfuscated and BitcoinJ"() {
+        when:
+        def toAddress = senderAddr
+        def txHex = builder.createSimpleSendHex(MSC, Coin.COIN.longValue())
+        def payload = hex(txHex)
+        Transaction tx = EncodeMultisig.encodeObfuscated(senderKey, payload, senderAddr.toString())
+        tx.addOutput(Coin.MILLICOIN, mpParams.exodusAddress)
+        tx.addOutput(Coin.CENT, toAddress)
+        Script script = ScriptBuilder.createInputScript(null)
+        def outPoint = new TransactionOutPoint(netParams, 1, Sha256Hash.create("boppitybop".getBytes()))
+
+        TransactionInput input = new TransactionInput(netParams, null, script.program, outPoint, null)
+        tx.addInput(input)
+        byte[] rawTx = tx.bitcoinSerialize()
+
+        then:
+        rawTx != null
+        rawTx.length > 20
+        rawTx.encodeHex().toString() == "01000000014fc80ee90baa51dcfa12d1a77dbd0e3820694c52327a056741c379eceb71df2e010000000100ffffffff03a0860100000000006751410401de173aa944eacf7e44e5073baca93fb34fe4b7897a1c82c92dfdc8a1f75ef58cd1b06e8052096980cb6e1ad6d3df143c34b3d7394bae2782a4df570554c2fb2103667d08778ca7e25f4044f96377023127ce00000000000000000000000000000052aea0860100000000001976a914946cb2e08075bcbaf157e47bcb67eb2b2339d24288ac40420f00000000001976a914ae38fb8f96d6a430feda6ccc5bbdc944c80832cc88ac00000000"
+    }
+
+    def "Build a transaction using OmniTxBuilder" () {
+        when:
+        def toAddress = senderAddr
+        // TODO: Are there mock UTXOs in bitcoinj?
+//        TransactionOutput utxo = new TransactionOutput(netParams, null, Coin.COIN, senderKey)
+//        List<TransactionOutput> utxos = [utxo]
+        List<TransactionOutput> utxos = []
+        def tx = omniTxBuilder.createSignedSimpleSend(senderKey, utxos, toAddress, MSC, 10000000)
+        byte[] rawTx = tx.bitcoinSerialize()
+
+        then:
+        rawTx != null
+        rawTx.length > 20
+
+        // TODO: Emtpy utxo list should throw an exception
+
+    }
+
+    private byte[] hex(String string) {
+        return string.decodeHex()
+    }
+
+}
