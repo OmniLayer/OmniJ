51d3433bfb8d10e5bc051c9e3908976ab3a7f2df
Sean Gilligan <sean@msgilligan.com>
2015-04-07 19:01:10 -0700

    Rename to OmniTxBuilderIntegSpec and clean up
    (Remove redundant test of send_MP)

--- omnij-rpc/src/integ/groovy/foundation/omni/test/tx/OmniTxBuilderIntegSpec.groovy
+++ omnij-rpc/src/integ/groovy/foundation/omni/test/tx/OmniTxBuilderIntegSpec.groovy
@@ -8,43 +8,23 @@ import org.bitcoinj.core.TransactionOutput
 import static foundation.omni.CurrencyID.MSC
 
 /**
- * Test simple sends using send_MP and OmniTXBuilder and sendRawTransaction
+ * Test OmniTXBuilder simple send using sendRawTransaction
  */
-class ClientSideRawTxSpec extends BaseRegTestSpec {
+class OmniTxBuilderIntegSpec extends BaseRegTestSpec {
     static final BigDecimal startBTC = 10.0
     static final BigDecimal startMSC = 1000.0
-
-    def "Can simple send amount MSC from one address to another using send_MP"() {
-        given:
-        def fundedAddress = createFundedAddress(startBTC, startMSC)
-        BigDecimal amount = 0.1
-
-        when: "we send MSC"
-        def startBalance = getbalance_MP(fundedAddress, MSC).balance
-        def toAddress = getNewAddress()
-        def txid = send_MP(fundedAddress, toAddress, MSC, amount)
-
-        and: "a block is generated"
-        generateBlock()
-        def endBalance = getbalance_MP(fundedAddress, MSC).balance
-
-        then: "the toAddress has the correct MSC balance and source address is reduced by correct amount"
-        amount == getbalance_MP(toAddress, MSC).balance
-        endBalance == startBalance - amount
-    }
+    static final omniTxBuilder = new OmniTxBuilder()
 
     def "Can simple send amount MSC from one address to another using OmniTxBuilder and sendraw RPC"() {
-        given:
-        def omniTxBuilder = new OmniTxBuilder()
+        given: "a fundedAddress with BTC/MSC and a newly created toAddress"
         def fundedAddress = createFundedAddress(startBTC, startMSC)
+        def startBalance = getbalance_MP(fundedAddress, MSC).balance
         def fundedKey = dumpPrivKey(fundedAddress)
+        def toAddress = getNewAddress()
+        List<TransactionOutput> utxos = listUnspentJ(fundedAddress)
         BigDecimal amount = 0.1
 
         when: "we build a signed Omni Simple Send Transaction in a bitcoinj Transaction object"
-        def startBalance = getbalance_MP(fundedAddress, MSC).balance
-        def toAddress = getNewAddress()
-
-        List<TransactionOutput> utxos = listUnspentJ(fundedAddress)
         def tx = omniTxBuilder.createSignedSimpleSend(fundedKey, utxos, toAddress, MSC, BTC.btcToCoin(amount).longValue())
 
         and: "we send it"
