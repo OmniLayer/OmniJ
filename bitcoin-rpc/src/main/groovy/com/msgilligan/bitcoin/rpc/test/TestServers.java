b04365733a7414f97a0d84cca54fff592bef58b6
Sean Gilligan <sean@msgilligan.com>
2015-04-16 19:08:57 -0700

    Move test configuration info to gradle.properties

--- bitcoin-rpc/src/main/groovy/com/msgilligan/bitcoin/rpc/test/TestServers.java
+++ bitcoin-rpc/src/main/groovy/com/msgilligan/bitcoin/rpc/test/TestServers.java
@@ -1,16 +1,9 @@
 package com.msgilligan.bitcoin.rpc.test;
 
-import com.msgilligan.bitcoin.rpc.RPCURI;
-
-import java.net.URI;
-import java.net.URISyntaxException;
-
 /**
- *
+ * Credentials to server under test.
  */
 public class TestServers {
-    // TODO: Password configurations for tests should come from external config
-    // In the meantime they should be collected here, and moved to getters
-    public static final String rpcTestUser = "bitcoinrpc";
-    public static final String rpcTestPassword = "pass";
+    public static final String rpcTestUser = System.getProperty("omni.test.rpcTestUser");
+    public static final String rpcTestPassword = System.getProperty("omni.test.rpcTestPassword");
 }
--- omnij-rpc/src/main/java/foundation/omni/rpc/test/TestServers.java
+++ omnij-rpc/src/main/java/foundation/omni/rpc/test/TestServers.java
@@ -9,13 +9,11 @@ import java.net.URISyntaxException;
  * Test Server Connection Data
  */
 public class TestServers {
-    // TODO: Password configurations for tests should come from external config
-    // In the meantime they should be collected here, and moved to getters
-    public static final String rpcTestUser = "bitcoinrpc";
-    public static final String rpcTestPassword = "pass";
-    public static final String stableOmniRpcHost = "core.stage.merchantcoin.net";
-    public static final String stableOmniRpcUser = "xmc-msc-rpc";
-    public static final String stableOmniRpcPassword = "emdERDIDE82934$%$";
+    public static final String rpcTestUser = System.getProperty("omni.test.rpcTestUser");
+    public static final String rpcTestPassword = System.getProperty("omni.test.rpcTestPassword");
+    public static final String stableOmniRpcHost = System.getProperty("omni.test.rpcTestUser");
+    public static final String stableOmniRpcUser = System.getProperty("omni.test.rpcTestUser");
+    public static final String stableOmniRpcPassword = System.getProperty("omni.test.rpcTestUser");
 
     public static URI getStablePublicMainNetURI() {
         try {
