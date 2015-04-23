eeb64a2bfcab9a59f3c03699deb3302d5240eec9
dexX7 <dexx@bitwatch.co>
2015-04-23 21:28:34 +0200

    Add RPC tests for block reorganizations
    The tests require a client, which is based on Bitcoin Core 0.10, as well as the following RPC extension "clearmempool":
    
    https://github.com/OmniLayer/omnicore/compare/omnicore-0.0.10...dexX7:oc-0.10-rpc-clearmempool?expand=1

---
+++ omnij-rpc/src/integ/groovy/foundation/omni/test/rpc/reorgs/BaseReorgSpec.groovy
@@ -0,0 +1,56 @@
+package foundation.omni.test.rpc.reorgs
+
+import com.msgilligan.bitcoin.rpc.JsonRPCException
+import foundation.omni.BaseRegTestSpec
+import org.bitcoinj.core.Sha256Hash
+import org.junit.internal.AssumptionViolatedException
+
+class BaseReorgSpec extends BaseRegTestSpec {
+
+    static protected BigDecimal startBTC = 0.1
+    static protected BigDecimal startMSC = 0.2
+
+    def setupSpec() {
+        try {
+            clearMemPool()
+        } catch(JsonRPCException ignored) {
+            throw new AssumptionViolatedException('The client has no "clearmempool" command')
+        }
+    }
+
+    Sha256Hash generateAndGetBlockHash() throws AssumptionViolatedException
+    {
+        List<String> result = generateBlock() as List<String>
+        if (result == null) {
+            throw new AssumptionViolatedException('The client is not based on Bitcoin Core 0.10')
+        }
+        assert result.size() > 0
+        return new Sha256Hash(result[0])
+    }
+
+    Boolean checkTransactionValidity(Sha256Hash txid, Integer confirmationsLimit = 1)
+    {
+        try {
+            def transaction = getRawTransaction(txid, true) as Map<String, Object>
+            if (transaction.confirmations < confirmationsLimit) {
+                return false
+            }
+        } catch(Exception ignored) {
+            return false
+        }
+        try {
+            def transaction = getTransactionMP(txid)
+            if (transaction.valid != true) {
+                return false
+            }
+            if (transaction.confirmations < confirmationsLimit) {
+                return false
+            }
+        } catch(Exception ignored) {
+            return false
+        }
+
+        return true
+    }
+
+}
