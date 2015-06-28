cd4cc401c153356a4c2de4e04f1f9496a18a4915
Sean Gilligan <sean@msgilligan.com>
2015-06-28 00:18:43 -0700

    Minor cleanup use == for tests

--- bitcoin-rpc/src/integ/groovy/com/msgilligan/bitcoin/rpc/BitcoinStepwiseSpec.groovy
+++ bitcoin-rpc/src/integ/groovy/com/msgilligan/bitcoin/rpc/BitcoinStepwiseSpec.groovy
@@ -8,23 +8,17 @@ class BitcoinStepwiseSpec extends BaseRegTestSpec {
     final static BigDecimal sendAmount = 10.0
     final static BigDecimal extraAmount = 0.10
 
-
     @Shared
     Address wealthyAddress
 
-    def setupSpec() {
-        // Create a new, unique address
-        wealthyAddress = getNewAddress()
-    }
-
-
     def "Be able to fund wealthy account from mining profits"() {
-        when: "we create a new account for Mastercoins and send some BTC to it"
+        when: "we send some BTC to an address"
+        wealthyAddress = getNewAddress()
         sendToAddress(wealthyAddress, 2*sendAmount + extraAmount)
         generateBlock()
 
         then: "we have the correct amount of BTC there"
-        getBitcoinBalance(wealthyAddress) >= 2*sendAmount + extraAmount
+        getBitcoinBalance(wealthyAddress) == 2*sendAmount + extraAmount
     }
 
     def "Send an amount to a newly created address"() {
@@ -33,15 +27,15 @@ class BitcoinStepwiseSpec extends BaseRegTestSpec {
         def testAmount = 1.0
 
         when: "we create a new address and send testAmount to it"
-        def destinationAddress = getNewAddress()
+        Address destinationAddress = getNewAddress()
         sendBitcoin(wealthyAddress, destinationAddress, testAmount)
         generateBlock()
 
         then: "the new address has a balance of testAmount"
         getBitcoinBalance(destinationAddress) == testAmount
 
-        and: "the source address is poorer"
-        getBitcoinBalance(wealthyAddress) <= wealthyStartBalance - testAmount
+        and: "the source address is poorer by the correct amount"
+        getBitcoinBalance(wealthyAddress) == wealthyStartBalance - testAmount - stdTxFee
     }
 
 }
