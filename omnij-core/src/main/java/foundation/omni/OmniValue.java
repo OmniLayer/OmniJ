59ee613531bfa319b4b29e2c9f07daecb5d69cad
Sean Gilligan <sean@msgilligan.com>
2015-08-17 13:41:32 -0700

    Remove lonely </code> tag

--- omnij-core/src/main/java/foundation/omni/OmniValue.java
+++ omnij-core/src/main/java/foundation/omni/OmniValue.java
@@ -9,7 +9,7 @@ import java.math.MathContext;
  * <p>Numeric value for a quantity of Omni tokens - base class for OmniDivisible and OmniIndivisible subclasses.
  * Known as "Number of Coins" in the Omni Protocol Specification.</p>
  *
- * <p>The internal representation is a <code>long</code> which </code> corresponds to what we call a
+ * <p>The internal representation is a <code>long</code> which corresponds to what we call a
  * "willet" in honour of J.R. Willet in the same fashion as the smallest Bitcoin unit is called a "satoshi".</p>
  *
  * <p>The constructors are <code>protected</code> and instances should be created with the <code>of()</code>
