c293fd6e08f4433519d6c217718f9545a89a5805
Sean Gilligan <sean@msgilligan.com>
2015-09-11 14:34:03 -0700

    Add MoneyMan test support class to omni-core

---
+++ omnij-core/src/main/java/foundation/omni/test/MoneyMan.java
@@ -0,0 +1,21 @@
+package foundation.omni.test;
+
+import foundation.omni.OmniDivisibleValue;
+import org.bitcoinj.core.Coin;
+
+/**
+ * Definitions for MoneyMan address functionality on TestNet and RegTest
+ */
+public class MoneyMan {
+    public static final long willetsPerSatoshi = 100;  // Exchange rate for MoneyMan: 100 Omni per BTC
+
+    /**
+     * Calculate Omni returned in a MoneyMan exchange.
+     *
+     * @param bitcoin An amount of bitcoin
+     * @return the amount of Omni the MoneyMan will exchange it for
+     */
+    public static OmniDivisibleValue toOmni(Coin bitcoin) {
+        return OmniDivisibleValue.ofWillets(bitcoin.value * willetsPerSatoshi);
+    }
+}
