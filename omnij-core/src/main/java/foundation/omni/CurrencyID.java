ba3983baf665bfcdf08004720c0c55ab44dede0b
Sean Gilligan <sean@msgilligan.com>
2016-04-11 09:18:24 -0700

    Add more popular CurrencyIDs, move towards using ticker symbols

--- omnij-core/src/main/java/foundation/omni/CurrencyID.java
+++ omnij-core/src/main/java/foundation/omni/CurrencyID.java
@@ -15,20 +15,30 @@ public final class CurrencyID implements Cloneable, Comparable<CurrencyID> {
     public static final long    BTC_VALUE = 0;
     public static final long    MSC_VALUE = 1;
     public static final long    TMSC_VALUE = 2;
+    public static final long    MAID_VALUE = 3; // MaidSafeCoin
+    /** @deprecated */
     public static final long    MaidSafeCoin_VALUE = 3;
     /** @deprecated */
     public static final long    TetherUS_VALUE = 31;
     public static final long    USDT_VALUE = 31;
+    public static final long    AMP_VALUE = 39; // Synerio
     public static final long    EURT_VALUE = 41;
+    public static final long    SEC_VALUE = 56; // SafeExchangeCoin
+    public static final long    AGRS_VALUE = 58; // Agoras
 
     public static final CurrencyID  BTC = new CurrencyID(BTC_VALUE);
     public static final CurrencyID  MSC = new CurrencyID(MSC_VALUE);
     public static final CurrencyID  TMSC = new CurrencyID(TMSC_VALUE);
-    public static final CurrencyID  MaidSafeCoin = new CurrencyID(MaidSafeCoin_VALUE);
+    public static final CurrencyID  MAID = new CurrencyID(MAID_VALUE);
+    /** @deprecated */
+    public static final CurrencyID  MaidSafeCoin = new CurrencyID(MAID_VALUE);
     /** @deprecated */
     public static final CurrencyID  TetherUS = new CurrencyID(USDT_VALUE);
     public static final CurrencyID  USDT = new CurrencyID(USDT_VALUE);
+    public static final CurrencyID  AMP = new CurrencyID(AMP_VALUE);
     public static final CurrencyID  EURT = new CurrencyID(EURT_VALUE);
+    public static final CurrencyID  SEC = new CurrencyID(SEC_VALUE);
+    public static final CurrencyID  AGRS = new CurrencyID(AGRS_VALUE);
 
     public static CurrencyID valueOf(String s) {
         switch (s) {
