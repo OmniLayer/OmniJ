96d3c3999c859707d06ff4c896587bf094116ef7
Sean Gilligan <sean@msgilligan.com>
2016-04-23 16:18:46 -0700

    Add Bittrex provider

---
+++ omnij-money/src/main/java/foundation/omni/money/BittrexXChangeRateProvider.java
@@ -0,0 +1,14 @@
+package foundation.omni.money;
+
+import com.msgilligan.bitcoinj.money.BaseXChangeExchangeRateProvider;
+import org.knowm.xchange.bittrex.v1.BittrexExchange;
+import org.knowm.xchange.currency.CurrencyPair;
+
+/**
+ *
+ */
+public class BittrexXChangeRateProvider extends BaseXChangeExchangeRateProvider {
+    public BittrexXChangeRateProvider() {
+        super(BittrexExchange.class, new CurrencyPair("OMNI", "BTC"), "OMNI", "BTC");
+    }
+}
