cbc08dca10c5d805b0f940c767bf717905b7f914
Sean Gilligan <sean@msgilligan.com>
2016-04-30 16:03:44 -0700

    Oops. missed file on last commit

--- omnij-rest-client/src/main/java/foundation/omni/rest/omniwallet/WalletBackupFile.java
+++ omnij-rest-client/src/main/java/foundation/omni/rest/omniwallet/WalletBackupFile.java
@@ -1,7 +1,25 @@
 package foundation.omni.rest.omniwallet;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
 /**
  *
  */
 public class WalletBackupFile {
+    public List<WalletBackupEntry> addresses;
+
+    public static WalletBackupFile read(File file) throws IOException {
+        ObjectMapper mapper = new ObjectMapper(); // can reuse, share globally
+        WalletBackupFile backup = mapper.readValue(file, WalletBackupFile.class);
+        return backup;
+    }
+
+    public static void save(File file, WalletBackupFile backup) throws IOException {
+        ObjectMapper mapper = new ObjectMapper(); // can reuse, share globally
+        mapper.writeValue(file, backup);
+    }
 }
