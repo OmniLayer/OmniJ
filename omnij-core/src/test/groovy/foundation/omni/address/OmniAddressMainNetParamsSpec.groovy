6809366926a980c34cbc4a849a79f2203937b66d
Sean Gilligan <sean@msgilligan.com>
2016-09-14 22:21:01 -0700

    Early proof-of-concept experiments with unique Omni ‘o’ and ‘Q’ addresses

---
+++ omnij-core/src/test/groovy/foundation/omni/address/OmniAddressMainNetParamsSpec.groovy
@@ -0,0 +1,38 @@
+package foundation.omni.address
+
+import org.bitcoinj.core.Address
+import org.bitcoinj.core.ECKey
+import org.bitcoinj.core.Utils
+import org.bitcoinj.params.MainNetParams
+import spock.lang.Specification
+
+/**
+ * Proof-of-concept test of ability to generate Omni-specific addresses
+ * via OmniAddressMainNetParams
+ */
+class OmniAddressMainNetParamsSpec extends Specification {
+    static final omniParams = OmniAddressMainNetParams.get()
+    static final btcParams = MainNetParams.get();
+
+    def "can create an omni address"() {
+        given: "A randomly generated ECKey"
+        def key = new ECKey()
+
+        when: "We generate an Omni address from it"
+        def omniAddress = key.toAddress(omniParams)
+
+        then: "It begins with an 'o'"
+        omniAddress.toString().substring(0,1) == 'o'
+    }
+
+    def "can create an omni P2SH address"() {
+        given: "An arbitrary hash value"
+        byte[] hash160 = Utils.sha256hash160([0] as byte[])
+
+        when: "We generate an Omni address from it"
+        def omniAddress = Address.fromP2SHHash(omniParams, hash160)
+
+        then: "It begins with an 'Q'"
+        omniAddress.toString().substring(0,1) == 'Q'
+    }
+}
