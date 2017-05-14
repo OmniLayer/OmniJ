ab6e692e6f9be8d718666e6b8aa7d8fd498165e0
Sean Gilligan <sean@msgilligan.com>
2017-05-13 21:19:37 -0700

    Add missing code from previous commit

--- omnij-rest-client/src/main/java/foundation/omni/rest/omniwallet/json/OmniwalletAddressBalance.java
+++ omnij-rest-client/src/main/java/foundation/omni/rest/omniwallet/json/OmniwalletAddressBalance.java
@@ -1,7 +1,22 @@
 package foundation.omni.rest.omniwallet.json;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.util.List;
+
 /**
- *
+ * Jackson POJO wrapper for raw Omniwallet address balance
  */
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class OmniwalletAddressBalance {
+    private final List<OmniwalletAddressPropertyBalance> balance;
+
+    public OmniwalletAddressBalance(@JsonProperty("balance") List<OmniwalletAddressPropertyBalance> balance) {
+        this.balance = balance;
+    }
+    
+    public List<OmniwalletAddressPropertyBalance> getBalance() {
+        return balance;
+    }
 }
