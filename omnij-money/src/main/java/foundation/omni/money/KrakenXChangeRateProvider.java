65d2b98befa3861bf3e33357f2e22eeab098d078
Sean Gilligan <sean@msgilligan.com>
2017-06-06 02:27:59 -0700

    Don’t enable USDT/USD on Kraken exchange yet

--- omnij-money/src/main/java/foundation/omni/money/KrakenXChangeRateProvider.java
+++ omnij-money/src/main/java/foundation/omni/money/KrakenXChangeRateProvider.java
@@ -4,10 +4,10 @@ import com.msgilligan.bitcoinj.money.BaseXChangeExchangeRateProvider;
 import org.knowm.xchange.kraken.KrakenExchange;
 
 /**
- * Bittrex wrapper
+ * Kraken wrapper
  */
 public class KrakenXChangeRateProvider extends BaseXChangeExchangeRateProvider {
     public KrakenXChangeRateProvider() {
-        super(KrakenExchange.class, "BTC/USD", "USDT/USD");
+        super(KrakenExchange.class, "BTC/USD" /*, "USDT/USD" */);
     }
 }
