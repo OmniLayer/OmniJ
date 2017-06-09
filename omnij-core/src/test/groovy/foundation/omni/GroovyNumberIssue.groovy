48adc894905ece6a6d873fac562c7156af4ea8bd
Sean Gilligan <sean@msgilligan.com>
2017-06-08 22:04:42 -0700

    Add test spec showing fixed Groovy numeric issue

---
+++ omnij-core/src/test/groovy/foundation/omni/GroovyNumberIssue.groovy
@@ -0,0 +1,29 @@
+package foundation.omni
+
+import spock.lang.Specification
+
+
+/**
+ * Tests that illustrate a (NOW FIXED) Groovy Bug
+ * See https://issues.apache.org/jira/browse/GROOVY-7608
+ */
+class GroovyNumberIssue extends Specification {
+    // Worked in Groovy 2.4.x
+    def "Equality works for integer values"() {
+        expect:
+        OmniDivisibleValue.of(1) == OmniDivisibleValue.of(1)
+    }
+
+    // Was broken in Groovy 2.4.x, fixed in 2.5.0-alpha-1 and later
+    def "Equality works for decimal values"() {
+        expect:
+        OmniDivisibleValue.of(1.1) == OmniDivisibleValue.of(1.1)
+    }
+
+    // Was broken in Groovy 2.4.x, fixed in 2.5.0-alpha-1 and later
+    def "Inequality works for integer/decimal values"() {
+        expect:
+        OmniDivisibleValue.of(1) != OmniDivisibleValue.of(1.1)
+    }
+
+}
