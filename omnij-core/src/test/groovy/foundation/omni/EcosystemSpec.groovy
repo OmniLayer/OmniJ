b7976ea25131c6a71858f38e6cc5c48db4995cb3
Sean Gilligan <sean@msgilligan.com>
2020-06-29 12:20:02 -0700

    Enable useJUnitPlaform(), Spock 2.0 fixes

--- omnij-core/src/test/groovy/foundation/omni/EcosystemSpec.groovy
+++ omnij-core/src/test/groovy/foundation/omni/EcosystemSpec.groovy
@@ -8,7 +8,7 @@ import spock.lang.Unroll
  */
 class EcosystemSpec extends Specification {
 
-    def "Real MSC Ecosystem has value 1"() {
+    def "Real Omni Ecosystem has value 1"() {
         when: "we create real ecosystem"
         Ecosystem ecosystem = Ecosystem.OMNI
 
@@ -18,7 +18,7 @@ class EcosystemSpec extends Specification {
         ecosystem.getValue() == (short) 1
     }
 
-    def "Test MSC Ecosystem has value 2"() {
+    def "Test Omni Ecosystem has value 2"() {
         when: "we create test ecosystem"
         Ecosystem ecosystem = Ecosystem.TOMNI
 
@@ -29,19 +29,19 @@ class EcosystemSpec extends Specification {
     }
 
     @Unroll
-    def "constructor is strongly typed and won't allow all Number subclasses (#id)"() {
+    def "constructor is strongly typed and won't allow all Number subclasses (#id)"(id) {
         when: "we try to create an ecosystem using an invalid numeric type"
         Ecosystem ecosystem = new Ecosystem(id)
 
         then: "exception is thrown"
-        groovy.lang.GroovyRuntimeException e = thrown()
+        GroovyRuntimeException e = thrown()
 
         where:
         id << [1F, 1.1F, 1.0D, 1.1D, 1.0, 1.1, 1.0G, 1.1G]
     }
 
     @Unroll
-    def "An Ecosystem can be represented as String (#id -> #ecosystemAsString)"() {
+    def "An Ecosystem can be represented as String (#ecosystem -> #ecosystemAsString)"(Ecosystem ecosystem, String ecosystemAsString) {
         expect:
         ecosystem.toString() == ecosystemAsString
 
