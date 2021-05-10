21df5e171fc1043f7841935810051b5a78887ea0
Sean Gilligan <sean@msgilligan.com>
2021-05-10 15:12:35 -0700

    download OmniCore BASH: Upgrade to v0.10.0
    also download from Github, not Bintray

--- test-download-omnicore-ubuntu.sh
+++ test-download-omnicore-ubuntu.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
-OMNICORE_HOST=https://bintray.com/artifact/download/omni/OmniBinaries
-OMNICORE_RELEASE=omnicore-0.8.2
-OMNICORE_FILE=$OMNICORE_RELEASE-x86_64-linux-gnu.tar.gz
+set -x
+OMNICORE_BASE=https://github.com/OmniLayer/omnicore/releases/download
+OMNICORE_VERSION=0.10.0
+TARGETPLATFORM=x86_64-linux-gnu
+OMNICORE_RELEASE=omnicore-${OMNICORE_VERSION}
+OMNICORE_FILE=omnicore-${OMNICORE_VERSION}-${TARGETPLATFORM}.tar.gz
+OMNICORE_URL=${OMNICORE_BASE}/v${OMNICORE_VERSION}/${OMNICORE_FILE}
 OMNICORE_HASH=13fad4537f98ab5356454436df6a808995c2c462563d4256a191fea0f10458e9
 
-wget "$OMNICORE_HOST/$OMNICORE_FILE"
-echo "$OMNICORE_HASH  $OMNICORE_FILE" | shasum --algorithm 256 --check
+wget "$OMNICORE_URL"
+echo "$OMNICORE_HASH $OMNICORE_FILE" | shasum --algorithm 256 --check
 mkdir -p copied-artifacts/src/
 tar zxvf $OMNICORE_FILE -C /tmp
 mv /tmp/$OMNICORE_RELEASE/bin/omnicored copied-artifacts/src/
