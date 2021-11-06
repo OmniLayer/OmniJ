69fab0781fc3d66bce77fb4b4b26b86e7aa82901
Sean Gilligan <sean@msgilligan.com>
2021-11-06 08:58:18 -0700

    OW RevisionInfo use `int` not `Integer` for lastBlock
    The replaces the usage of the Nonnull annotation

--- omnij-net-api/src/main/java/foundation/omni/netapi/omniwallet/json/RevisionInfo.java
+++ omnij-net-api/src/main/java/foundation/omni/netapi/omniwallet/json/RevisionInfo.java
@@ -4,8 +4,6 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import org.bitcoinj.core.Sha256Hash;
 
-import javax.annotation.Nonnull;
-
 /**
  * Jackson POJO wrapper for raw Omniwallet response
  */
@@ -13,12 +11,12 @@ import javax.annotation.Nonnull;
 public class RevisionInfo {
     private final Sha256Hash blockHash;
     private final String blockTime;
-    private final @Nonnull Integer lastBlock;
+    private final int lastBlock;
     private final String lastParsed;
 
     public RevisionInfo(@JsonProperty("block_hash") Sha256Hash blockHash,
                         @JsonProperty("block_time") String blockTime,
-                        @Nonnull @JsonProperty("last_block") Integer lastBlock,
+                        @JsonProperty("last_block") int lastBlock,
                         @JsonProperty("last_parsed") String lastParsed) {
         // Null check here?
         this.blockHash = blockHash;
@@ -33,7 +31,7 @@ public class RevisionInfo {
         return blockTime;
     }
 
-    public @Nonnull Integer getLastBlock() {
+    public int getLastBlock() {
         return lastBlock;
     }
 
