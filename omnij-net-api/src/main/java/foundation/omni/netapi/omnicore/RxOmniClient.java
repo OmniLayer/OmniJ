ceb7789f99ac47efab85338f077035380868d052
Sean Gilligan <sean@msgilligan.com>
2021-11-11 10:31:04 -0800

    RxOmniClient - use Rx support from superclass

--- omnij-net-api/src/main/java/foundation/omni/netapi/omnicore/RxOmniClient.java
+++ omnij-net-api/src/main/java/foundation/omni/netapi/omnicore/RxOmniClient.java
@@ -1,22 +1,16 @@
 package foundation.omni.netapi.omnicore;
 
 import foundation.omni.rpc.OmniClient;
-import io.reactivex.rxjava3.core.Flowable;
 import org.bitcoinj.core.NetworkParameters;
-import org.consensusj.bitcoin.json.pojo.ChainTip;
-import org.consensusj.bitcoin.rx.ChainTipService;
-import org.consensusj.bitcoin.rx.jsonrpc.PollingChainTipServiceImpl;
-import org.consensusj.bitcoin.rx.jsonrpc.RxJsonChainTipClient;
-import org.consensusj.bitcoin.rx.zeromq.RxBitcoinZmqService;
 
 import javax.net.ssl.SSLSocketFactory;
 import java.net.URI;
 
 /**
- *
+ * Add OmniProxyMethods to OmniClient (which is now already reactive)
+ * TODO: add OmniProxy support to OmniClient and remove this class
  */
-public class RxOmniClient extends OmniClient implements RxJsonChainTipClient, OmniProxyMethods {
-    ChainTipService chainTipService;
+public class RxOmniClient extends OmniClient implements OmniProxyMethods {
     private final boolean isOmniProxy;
 
     public RxOmniClient(NetworkParameters netParams, URI server, String rpcuser, String rpcpassword) {
@@ -28,21 +22,8 @@ public class RxOmniClient extends OmniClient implements RxJsonChainTipClient, Om
     }
 
     public RxOmniClient(SSLSocketFactory sslSocketFactory,  NetworkParameters netParams, URI server, String rpcuser, String rpcpassword, boolean useZmq, boolean isOmniProxy) {
-        super(sslSocketFactory, netParams, server, rpcuser, rpcpassword);
+        super(sslSocketFactory, netParams, server, rpcuser, rpcpassword, useZmq);
         this.isOmniProxy = isOmniProxy;
-        if (useZmq) {
-            chainTipService = new RxBitcoinZmqService(this);
-        } else {
-            // TODO: Add ability to Set polling interval and/or enable/disable polling
-            PollingChainTipServiceImpl pollingChainTipService = new PollingChainTipServiceImpl(this);
-            pollingChainTipService.start();
-            chainTipService = pollingChainTipService;
-        }
-    }
-
-    @Override
-    public Flowable<ChainTip> chainTipPublisher() {
-        return Flowable.fromPublisher(chainTipService.chainTipPublisher());
     }
 
     @Override
