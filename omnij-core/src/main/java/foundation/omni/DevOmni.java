6bc4e2b6dac281be6e83da61a93658e9b9736d31
Sean Gilligan <sean@msgilligan.com>
2021-12-16 14:27:02 -0800

    Further W.I.P on DevOmni calculations
    Calculations now *seem* to match the current Omni Core C++ calculations,
    but more analysis and testing is needed.

--- omnij-core/src/main/java/foundation/omni/DevOmni.java
+++ omnij-core/src/main/java/foundation/omni/DevOmni.java
@@ -10,15 +10,14 @@ import java.time.Instant;
  * Work-in-progress constants and functions for calculating Dev OMNI.
  * <p>
  * We use {@link Instant} and {@link Duration} but do not use other Java Time functions
- * because the Omni Specification specifies a precise value for {@link DevOmni#SECONDS_PER_YEAR}.
- * This is why the "years since" functions return {@link BigDecimal}.
+ * because the Omni Specification provides a specific value for {@link DevOmni#SECONDS_PER_YEAR}.
+ * This is why the {@link DevOmni#yearsSinceCrowdSale} function returns a {@link BigDecimal}.
  * <p>
- * TODO: More precise calculation of percentVested than `double`?
+ * See {@code calculate_and_update_devmsc()} in {@code omnicore.cpp}
+ * <p>
  * TODO: Verify math versus Omni Core C++ implementation (regtest and/or functional test vs mainnet?)
- * TODO: Functions for calculating Dev Omni issued in a specific block (by block timestamp?)
  */
 public class DevOmni {
-    private static final BigDecimal TWO = BigDecimal.valueOf(2);
     /**
      * Unix timestamp of End of the Exodus Crowd Sale as defined in the Omni Specification
      */
@@ -28,49 +27,87 @@ public class DevOmni {
      */
     public static final Duration SECONDS_PER_YEAR = Duration.ofSeconds(31_556_926);
     /**
-     * The factor 0.5, as used in the 1 - 0.5 ** y calculation in the Omni Specification
+     * The factor 2.0, the inverse of the 0.5 used in the {@code 1 - 0.5 ** y} calculation in the Omni Specification
      */
-    public static final BigDecimal VESTING_FACTOR = BigDecimal.ONE.divide(TWO, MathContext.UNLIMITED);
-
+    public static final BigDecimal VESTING_FACTOR = BigDecimal.valueOf(2);
     /**
-     * @return Fractional years since end-of-Exodus based on system time
+     * Total number of Dev OMNI (as specified by the {@code all_reward} constant in {@code omnicore.cpp})
      */
-    public static BigDecimal yearsSinceCrowdSale() {
-        return yearsSinceCrowdSale(Instant.now());
-    }
+    public static final OmniDivisibleValue ALL_DEV_OMNI = OmniDivisibleValue.ofWilletts(56_316_235_762_22L);
+    /**
+     * APPROXIMATE Total number of OMNI issued in Crowdsale
+     */
+    public static final OmniDivisibleValue CROWDSALE_OMNI = OmniDivisibleValue.ofWilletts(563_202_059_032_29L);
+
+    private static final BigDecimal secondsPerYearBD = BigDecimal.valueOf(SECONDS_PER_YEAR.getSeconds());
+    private static final double vestingFactorDouble = VESTING_FACTOR.doubleValue();
 
     /**
      * @return Fractional years since end-of-Exodus based on system time
      * @param time end of time interval (e.g. timestamp of a particular block)
      */
     public static BigDecimal yearsSinceCrowdSale(Instant time) {
-        BigDecimal secsSinceExodus = BigDecimal.valueOf(time.getEpochSecond() - EXODUS_END_SECS.getEpochSecond());
-        BigDecimal secondsPerYear = BigDecimal.valueOf(SECONDS_PER_YEAR.getSeconds());
-        return secsSinceExodus.divide(secondsPerYear, RoundingMode.HALF_UP);
+        BigDecimal secsSinceExodus = new BigDecimal(time.getEpochSecond() - EXODUS_END_SECS.getEpochSecond(), MathContext.UNLIMITED);
+        return secsSinceExodus.divide(secondsPerYearBD, RoundingMode.HALF_UP);
     }
 
     /**
      * @return APPROXIMATE percent vested (possibly different from Omni Core calculations due to rounding)
      */
-    public static double percentVested() {
-        return percentVested(Instant.now());
+    public static BigDecimal percentVested(Instant time) {
+        return BigDecimal.ONE.subtract(percentUnvested(time), MathContext.UNLIMITED);
     }
 
     /**
-     * @return APPROXIMATE percent vested (possibly different from Omni Core calculations due to rounding)
+     * @return APPROXIMATE percent unvested (possibly different from Omni Core calculations due to rounding)
      */
-    public static double percentVested(Instant time) {
-        return 1.0 - Math.pow(VESTING_FACTOR.doubleValue(), yearsSinceCrowdSale(time).doubleValue());
+    public static BigDecimal percentUnvested(Instant time) {
+        BigDecimal divisor = unvestedDivisor(time);
+        return BigDecimal.ONE.divide(divisor, MathContext.UNLIMITED);
     }
 
     /**
-     * Calculate timestamp using {@link DevOmni#SECONDS_PER_YEAR}
+     * Calculate APPROXIMATE vested amount of Dev OMNI
+     *
+     * @param time Timestamp
+     * @return Amount of vested OMNI
+     */
+    public static OmniDivisibleValue omniVested(Instant time) {
+        return OmniDivisibleValue.ofWilletts(ALL_DEV_OMNI.willetts - omniUnvested(time).willetts);
+    }
+
+    /**
+     * Calculate APPROXIMATE unvested amount of Dev OMNI
+     *
+     * @param time Timestamp
+     * @return Amount of unvested OMNI
+     */
+    public static OmniDivisibleValue omniUnvested(Instant time) {
+        BigDecimal divisor = unvestedDivisor(time);
+        BigDecimal unvested = BigDecimal.valueOf(ALL_DEV_OMNI.willetts).divide(divisor, RoundingMode.HALF_UP);
+        return OmniDivisibleValue.ofWilletts(unvested.longValue());
+    }
+
+    /**
+     * @return APPROXIMATE total OMNI tokens at a given block time
+     */
+    public static OmniDivisibleValue totalOmniTokens(Instant time) {
+        return CROWDSALE_OMNI.plus(omniVested(time));
+    }
+    
+    /**
+     * Calculate the APPROXIMATE denominator/divisor used in Dev OMNI/MSC calculations.
      * <p>
-     * (package-private for use internally and in tests)
-     * @param years years to add
-     * @return epoch value
+     * In some contexts the numerator may be a {@code 1} and in others it may be a
+     * quantity of Dev OMNI.
+     * <p>
+     * TODO: Figure out how to divide by fractional years with more precision than double
+     * @param time Timestamp used for vesting calculation
+     * @return The denominator, n to use for calculating unvested percentage or amount
      */
-    static Instant addYears(int years) {
-        return EXODUS_END_SECS.plusSeconds(years * SECONDS_PER_YEAR.getSeconds());
+    private static BigDecimal unvestedDivisor(Instant time) {
+        // Built-in JDK math only allows `pow()` with fractional exponents for `double` type
+        double years = yearsSinceCrowdSale(time).doubleValue();
+        return BigDecimal.valueOf(Math.pow(vestingFactorDouble, years));
     }
 }
--- omnij-core/src/test/groovy/foundation/omni/DevOmniSpec.groovy
+++ omnij-core/src/test/groovy/foundation/omni/DevOmniSpec.groovy
@@ -5,17 +5,46 @@ import spock.lang.Specification
 import java.time.Instant
 
 /**
- *
+ * Preliminary tests for DevOmni (aka Dev MSC) issuance
  */
 class DevOmniSpec extends Specification {
 
-    def "verify dev OMNI vesting after n years"(int years, BigDecimal vesting) {
+    def "ALL DEV OMNI is correct value"() {
+        expect:
+        DevOmni.ALL_DEV_OMNI.willetts == 56_316_235_762_22L
+    }
+
+    // NOTE: This value has not been verified against the actual Crowdsale totals!!
+    def "CROWDSALE_OMNI is correct value"() {
+        expect:
+        DevOmni.CROWDSALE_OMNI.willetts == 563_202_059_032_29L
+    }
+
+    def "verify dev OMNI unvested % after n years"(int years, BigDecimal expected) {
         when:
-        Instant end = DevOmni.addYears(years)
-        double vested = DevOmni.percentVested(end)
+        Instant end = addYears(years)
+        BigDecimal unvested = DevOmni.percentUnvested(end)
 
         then:
-        vested == vesting.doubleValue()
+        unvested == expected
+
+        where:
+        years   | expected
+        0       | 1.0
+        1       | 0.50
+        2       | 0.25
+        3       | 0.125
+        10      | 0.0009765625
+        20      | 0.00000095367431640625
+    }
+
+    def "verify dev OMNI vesting after n years"(int years, BigDecimal vesting) {
+        when:
+        Instant end = addYears(years)
+        BigDecimal vested = DevOmni.percentVested(end)
+
+        then:
+        vested == vesting
 
         where:
         years   | vesting
@@ -24,6 +53,85 @@ class DevOmniSpec extends Specification {
         2       | 0.75
         3       | 0.875
         10      | 0.9990234375
-        20      | 0.9999990463256836
+        20      | 0.99999904632568359375
+    }
+    
+    def "verify dev OMNI vested after n years"(int years, long expectedVested) {
+        when:
+        Instant end = addYears(years)
+        OmniDivisibleValue vested = DevOmni.omniVested(end)
+
+        then:
+        vested.willetts == expectedVested
+
+        where:
+        years   | expectedVested
+        0       | 0.0
+        1       | 0.5d * DevOmni.ALL_DEV_OMNI.willetts
+        2       | 0.75d * DevOmni.ALL_DEV_OMNI.willetts
+        3       | 0.875d * DevOmni.ALL_DEV_OMNI.willetts
+        10      | 0.9990234375d * DevOmni.ALL_DEV_OMNI.willetts
+        20      | 0.9999990463256836d * DevOmni.ALL_DEV_OMNI.willetts
+    }
+
+    def "verify OMNI vesting at timestamp"(int timestamp, BigDecimal vesting) {
+        when:
+        Instant end = Instant.ofEpochSecond(timestamp)
+        BigDecimal vested = DevOmni.percentVested(end)
+
+        then:
+        vested == vesting
+
+        where:
+        block       | timestamp       | vesting
+        714162      | 1639510432      | 0.99609375
+        714163      | 1639512841      | 0.99609375
+        714164      | 1639513378      | 0.99609375
+        714165      | 1639513483      | 0.99609375
+        714166      | 1639514670      | 0.99609375
+    }
+
+    def "verify total Dev OMNI at timestamp"(int timestamp, long vesting) {
+        when:
+        Instant end = Instant.ofEpochSecond(timestamp)
+        long vested = DevOmni.omniVested(end).willetts
+
+        then:
+        vested == vesting
+
+        where:
+        block       | timestamp       | vesting
+        714162      | 1639510432      | 56_096_250_466_27
+        714163      | 1639512841      | 56_096_250_466_27
+        714164      | 1639513378      | 56_096_250_466_27
+        714165      | 1639513483      | 56_096_250_466_27
+        714166      | 1639514670      | 56_096_250_466_27
+    }
+
+    def "verify total Tokens for timestamp"(int block, int timestamp, long totalTokens) {
+        when:
+        def calcTotal = DevOmni.totalOmniTokens(Instant.ofEpochSecond(timestamp))
+
+        then:
+        calcTotal.willetts == totalTokens
+
+        where:
+        block   | timestamp     | totalTokens
+        714162  | 1639510432    | 619_298_309_498_56
+        714163  | 1639512841    | 619_298_309_498_56
+        714164  | 1639513378    | 619_298_309_498_56
+        714165  | 1639513483    | 619_298_309_498_56
+        714166  | 1639514670    | 619_298_309_498_56
+     // 714167  | 1639510432    | 619_298_321_579_90 // This calculation doesn't handle time that goes backward (i.e. via high-water mark)
+    }
+
+    /**
+     * Calculate timestamp using {@link DevOmni#SECONDS_PER_YEAR}
+     * <p>
+     * @param years years to add
+     * @return epoch value
+     */
+    private static Instant addYears(int years) {
+        return DevOmni.EXODUS_END_SECS.plusSeconds(years * DevOmni.SECONDS_PER_YEAR.getSeconds());
     }
 }
