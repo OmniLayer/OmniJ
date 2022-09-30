4aa44abde52ee93e973b1cac6e40e0bbbc3fe0a3
Sean Gilligan <sean@msgilligan.com>
2022-09-30 06:53:10 -0700

    Deprecate OmniClientDelegate trait
    It was only used in BaseMainNetSpec which can just use the @Delegate
    annotation directly.

--- omnij-rpc/src/integ/groovy/foundation/omni/BaseMainNetSpec.groovy
+++ omnij-rpc/src/integ/groovy/foundation/omni/BaseMainNetSpec.groovy
@@ -3,9 +3,7 @@ package foundation.omni
 import foundation.omni.rpc.OmniClient
 import groovy.util.logging.Slf4j
 import org.consensusj.bitcoin.jsonrpc.groovy.BlockchainDotInfoSyncing
-import org.consensusj.jsonrpc.JsonRpcStatusException
 import org.consensusj.bitcoin.jsonrpc.RpcURI
-import foundation.omni.rpc.OmniClientDelegate
 import foundation.omni.rpc.test.TestServers
 import org.bitcoinj.params.MainNetParams
 import spock.lang.Specification
@@ -20,21 +18,30 @@ import spock.lang.Specification
  *
  */
 @Slf4j
-abstract class BaseMainNetSpec extends Specification implements OmniClientDelegate,
-        BlockchainDotInfoSyncing {
+abstract class BaseMainNetSpec extends Specification implements BlockchainDotInfoSyncing {
     static final protected TestServers testServers = TestServers.instance
     static final protected String rpcTestUser = testServers.rpcTestUser
     static final protected String rpcTestPassword = testServers.rpcTestPassword;
-    {
-        client = new OmniClient(MainNetParams.get(), RpcURI.defaultMainNetURI, rpcTestUser, rpcTestPassword)
-    }
     static final Integer rpcWaitTimeoutSeconds = 10*60*60  // Wait up to 10 (!) hours for RPC response
+    private static OmniClient INSTANCE;
+
+    @Delegate
+    OmniClient client() {
+        if (INSTANCE == null) {
+            INSTANCE = new OmniClient(MainNetParams.get(), RpcURI.defaultMainNetURI, rpcTestUser, rpcTestPassword)
+        }
+        return INSTANCE;
+    }
+
+    OmniClient getClient() {
+        return client()
+    }
 
     /**
      * Wait for RPC server to be responding and to be in sync with the Bitcoin Blockchain
      */
     void setupSpec() {
-        Boolean available = client.waitForServer(rpcWaitTimeoutSeconds)
+        Boolean available = client().waitForServer(rpcWaitTimeoutSeconds)
         if (!available) {
             log.error "Timeout error."
         }
@@ -45,20 +52,9 @@ abstract class BaseMainNetSpec extends Specification implements OmniClientDelega
         //
         waitForSync(client);
 
-        def info = client.getinfo()
-
-        def omniVersion
-        def infoMP
-        try {
-            infoMP = client.omniGetInfo()
-        } catch (JsonRpcStatusException e) {
-            log.error "Exception calling omniGetInfo()"
-        }
-        if (infoMP?.mastercoreversion) {
-            omniVersion = infoMP.mastercoreversion.toString()
-        } else {
-            omniVersion = info.mastercoreversion
-        }
+        var info = client().getNetworkInfo()
+        var omniInfo = client().omniGetInfo()
+        var omniVersion = omniInfo.omnicoreversion
         log.info "Bitcoin version: ${info.version}"
         log.info "Omni Core version: ${omniVersion}"
     }
--- omnij-rpc/src/main/groovy/foundation/omni/rpc/OmniClientDelegate.groovy
+++ omnij-rpc/src/main/groovy/foundation/omni/rpc/OmniClientDelegate.groovy
@@ -2,7 +2,9 @@ package foundation.omni.rpc
 
 /**
  * Groovy trait for adding a OmniClient delegate to any class
+ * @deprecated Use {@code @Delegate OmniClient client} directly
  */
+@Deprecated
 trait OmniClientDelegate {
     @Delegate
     OmniClient client
