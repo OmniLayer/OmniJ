baab464ae27b9d6694e635b270076bea688baae4
Sean Gilligan <sean@msgilligan.com>
2022-10-11 08:22:52 -0700

    RegTestOmniFundingSource: simplify/cleanup

--- omnij-rpc/src/main/groovy/foundation/omni/test/RegTestOmniFundingSource.groovy
+++ omnij-rpc/src/main/groovy/foundation/omni/test/RegTestOmniFundingSource.groovy
@@ -1,8 +1,6 @@
 package foundation.omni.test
 
 import foundation.omni.rpc.test.OmniTestClient;
-import foundation.omni.rpc.test.OmniTestClientAccessor
-import org.consensusj.bitcoin.jsonrpc.test.FundingSource;
 import org.consensusj.bitcoin.jsonrpc.test.RegTestFundingSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -11,21 +9,10 @@ import org.slf4j.LoggerFactory;
  * This should be converted to Java.
  * So let's keep it in Java-like Groovy until then
  */
-public class RegTestOmniFundingSource extends RegTestFundingSource implements OmniTestClientAccessor, OmniTestSupport, OmniFundingSource  {
-    private static final Logger log = LoggerFactory.getLogger(RegTestFundingSource.class);
-    RegTestFundingSource fundingSource;
+public class RegTestOmniFundingSource extends RegTestFundingSource {
+    private static final Logger log = LoggerFactory.getLogger(RegTestOmniFundingSource.class);
 
-    public RegTestOmniFundingSource(OmniTestClient client, RegTestFundingSource fundingSource) {
-        super(client);          // Set BitcoinExtendedClient in superclass
-        this.fundingSource = fundingSource;
-    }
-
-    OmniTestClient client() {
-        return client;
-    }
-
-    @Override
-    FundingSource fundingSource() {
-        return fundingSource
+    public RegTestOmniFundingSource(OmniTestClient client) {
+        super(client);
     }
 }
